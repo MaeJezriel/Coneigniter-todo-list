857c7dd31ae607d63f76d43e6704e6470f4e032e
MaeJezriel <201810487@gordoncollege.edu.ph>
2022-05-12 14:40:02 +0800

    update for ui

--- app/Views/create-todo.php
+++ app/Views/create-todo.php
@@ -7,15 +7,77 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.0/jquery.validate.js"></script>  
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.0/additional-methods.min.js"></script>
 <link rel = "stylesheet" type = "text/css" href = "<?php echo base_url(); ?>css/style.css">
+
+<style>
+
+  .card{
+    margin: 0 auto; /* Added */
+    float: none; /* Added */
+    margin-bottom: 10px; /* Added */
+    margin-top: 50px;   
+  }
+  
+ 
+
+  h5{
+    color: #0047AB;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+      Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+  }
+  
+
+  .card-title{
+    color: #0047AB;
+    font-size: 40px;
+    font-weight: 900;
+  }
+
+  .card-text{
+    color: #0047AB;
+    font-weight: bold;
+    font-size: 20px;
+  }
+
+  label{
+    color: #1434A4;
+    font-weight: bold;
+    margin-left: 0px;
+  }
+
+ .form-group{
+      width: 90%;
+      margin-left: 40px;
+  }
+
+  .form-group{
+      width: 90%;
+      height: 100
+      margin-left: 40px;
+  }
+
+
+
+</style>
+
 </head>
-<body>
-<div class="container">
-<br>
-<?= \Config\Services::validation()->listErrors(); ?>
-<span class="d-none alert alert-success mb-3" id="res_message"></span>
-<div class="row">
-<div class="col-md-9">
-<form action="<?php echo base_url('/todos/store');?>" name="user_create" id="todo_create" method="post" accept-charset="utf-8">
+
+
+
+
+<div class="card" style="width: 50rem;">
+
+
+  <!-- <img class="card-img-top" src="..." alt="Card image cap"> -->
+  <div class="card-body">
+    
+    <h5 class="card-title">Create Task</h5>
+    <!-- <p class="card-text">May</p> -->
+ 
+
+   
+  </div>
+
+  <form action="<?php echo base_url('/todos/store');?>" name="user_create" id="todo_create" method="post" accept-charset="utf-8">
 <div class="form-group">
 <label for="formGroupExampleInput">Title</label>
 <input type="text" name="title" class="form-control" id="formGroupExampleInput" placeholder="Please enter Title">
@@ -28,9 +90,17 @@
 <button type="submit" id="send_form" class="btn btn-success">Submit</button>
 </div>
 </form>
+  <!-- <div class="card-body">
+    <a href="#" class="card-link">Card link</a>
+    <a href="#" class="card-link">Another link</a>
+  </div> -->
+ 
 </div>
-</div>
-</div>
+
+
+
+
+
 <script>
 </script>
 </body>
--- app/Views/todos.php
+++ app/Views/todos.php
@@ -1,11 +1,58 @@
 <!doctype html>
 <html lang="en">
   <head>
+  <style>
+
+  .card{
+    margin: 0 auto; /* Added */
+    float: none; /* Added */
+    margin-bottom: 10px; /* Added */
+    margin-top: 50px;
+    border-radius: 25px;
+    
+  }
+  
+  .btn{
+    float: right;
+    margin-left: 10px;
+  }
+
+  .addbtn{
+    margin-bottom:-100px;
+
+  }
+
+  h5{
+    color: #0047AB;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+      Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+  }
+  
+
+  .card-title{
+    color: #0047AB;
+    font-size: 40px;
+    font-weight: 900;
+  }
+
+  .card-text{
+    color: #0047AB;
+    font-weight: bold;
+    font-size: 20px;
+  }
+
+</style>
+
+
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <title>Todo List</title>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-
+  <!-- Bootstrap icons -->
+  <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css"
+    />  
    <!-- As a heading -->
 <nav class="navbar navbar-light bg-light">
   <span class="navbar-brand mb-0 h1">To-Do List</span>
@@ -13,42 +60,42 @@
 
 </head>
 <body>
+  
  
-<div class="container mt-5">
-    <a href="<?php echo site_url('todos/create') ?>" class="btn btn-success mb-2">Create</a>
+<div class="card" style="width: 50rem;">
+
+
+  <!-- <img class="card-img-top" src="..." alt="Card image cap"> -->
+  <div class="card-body">
+    
+    <h5 class="card-title">Thursday, 12</h5>
+    <p class="card-text">May</p>
+    <a href="<?php echo site_url('todos/create') ?>" class="btn btn-success mb-2">Create</a> 
+
     <?php
      if(isset($_SESSION['msg'])){
         echo $_SESSION['msg'];
-      }
-     ?>
-
-  <div class="row mt-3">
-      <!--Row for todolist-->
-     <div class="row" >
-   <?php if($todos): ?>
-   <?php foreach($todos as $todo): ?>
-  <div class="col-sm-6">
-    <div class="card">
-      <div class="card-body">
-        <h5 class="card-title"><?php echo $todo['id']; ?></h5>
-        <p class="card-text"><?php echo $todo['title']; ?></p>
-        <p class="card-text"><?php echo $todo['description']; ?></p>
-        <a href="<?php echo base_url('todos/edit/'.$todo['id']);?>" class="btn btn-success">Edit</a>
-              <a href="<?php echo base_url('todos/delete/'.$todo['id']);?>" class="btn btn-danger">Delete</a>
-      </div>
-    </div>
-    <br>
+      }?>
   </div>
-  
+
+  <ul class="list-group list-group-flush">
+    <br>
+  <?php if($todos): ?>
+   <?php foreach($todos as $todo): ?>
+    <li class="list-group-item"><?php echo $todo['title']; ?>
+    <a href="<?php echo base_url('todos/edit/'.$todo['id']);?>" class="btn btn-success">Edit</a>
+    <a href="<?php echo base_url('todos/delete/'.$todo['id']);?>" class="btn btn-danger">Delete</a>
+  </li>
+    
+  </ul>
+  <!-- <div class="card-body">
+    <a href="#" class="card-link">Card link</a>
+    <a href="#" class="card-link">Another link</a>
+  </div> -->
   <?php endforeach; ?>
    <?php endif; ?>
-
-
 </div>
 
-  </div>
-</div>
- 
 <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
  
 <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.20/css/jquery.dataTables.min.css">
@@ -59,6 +106,7 @@
     $(document).ready( function () {
       $('#todos').DataTable();
   } );
+  
 </script>
 </body>
 </html>
